27989460370478ef886efe85db3b92a3d7fb07f8
feigebaobei <18515195415@163.com>
2022-03-07 23:13:37 +0800

    feat(levelCert): init查询结果页面

--- src/pages/Query/LevelCert/index.js
+++ src/pages/Query/LevelCert/index.js
@@ -1,13 +1,28 @@
-// import './index.css';
+import './index.css';
 import {useState} from 'react'
+import Footer from '../../../components/Footer'
 
 let {log} = console // for test
 
 function LevelCert() {
+  // let [levelCertUrl, setLevelCertUrl] = useState('//localhost:4000/images/third164654178193118342.jpeg')
+  let [levelCertUrl, setLevelCertUrl] = useState('')
+  
   return (
-    <div>
-        string of LevelCert
-    </div>
+    <section className='levelCertBox'>
+        <img src={require('../../../access/logo.gif')} className="bannerImg" alt="logo" />
+        <p className="path">
+          <span className="homePage pathItem">首页</span>&gt;<span className="pathItem">级位证查询</span>&gt;<span className="pathItem">查询结果</span>
+        </p>
+        {levelCertUrl ? 
+        <article className="hasPic">
+          <img src={levelCertUrl} alt="证书图片" />
+        </article> :
+        <article className="noPic">
+          <p>暂无资料！</p>
+        </article>}
+        <Footer></Footer>
+    </section>
   );
 }
 
--- src/pages/Query/index.js
+++ src/pages/Query/index.js
@@ -3,6 +3,7 @@ import {useState,
   useRef
 } from 'react'
 import api from '../../utils/api'
+import Footer from '../../components/Footer'
 
 let buttonList = [
   { label: '级位证', value: '10'},
@@ -95,12 +96,13 @@ function Query() {
           </div>
         </form>
       </div>
-      <footer className="footer">
+      {/* <footer className="footer">
         <img src={require('../../access/logo.png')} className="footerLogo" alt="logo" />
         <p className="footerP">版权所有  COPYRIGHT  2005-2019 武术段位推广中心</p>
         <p className="footerP">中国最权威的武术证书认证官网-中国武术协会证书认证中心</p>
         <p className="footerP">提供段位查询、级位查询、教练查询等武术类相关的证书查询服务，并提供国内外及协会相关新闻资讯</p>
-      </footer>
+      </footer> */}
+      <Footer></Footer>
     </div>
   );
 }
